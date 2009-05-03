6cbcaca5bcd76d64fad967df3094bbcaf9b51484
Martin Duquesnoy <xorg62@gmail.com>
2009-05-03 15:11:51 +0200

    Add irc.c & util.c -> send_msg() and init_connection()

--- global.h
+++ global.h
@@ -9,14 +9,23 @@
 #ifndef _GLOBAL_H
 #define _GLOBAL_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
 #include <string.h>
+#include <regex.h>
+#include <netdb.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-#define IRC_NICK  volodim
-#define IRC_SERV  irc.freenode.net
+#define IRC_NICK  "uchat_test"
+#define IRC_NAME  "Uchat test"
+#define IRC_SERV  "irc.freenode.net"
+#define IRC_CHAN  "#uchat"
+#define IRC_PORT  6667
 
 /*
  * Prototypes
@@ -25,4 +34,16 @@
 /* gui.c */
 int init_screen(void);
 
-#endif
+/* irc.c */
+void init_connection(void);
+
+/* util.c */
+void send_msg(int sock, char* format, ...);
+
+
+/*
+ * Variables
+ */
+int Socket;
+
+#endif /* GLOBAL_H */
